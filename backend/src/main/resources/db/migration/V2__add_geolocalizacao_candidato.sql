300d8d2ca9a08d52e7e58779723b50d06709b754
Júlio Cézar <juliocezar.zetta@gmail.com>
2026-06-28 11:32:03 -0300

    Consertado erros do flyway em relação a comando exclusivos do MySQL

--- backend/src/main/resources/db/migration/V2__add_geolocalizacao_candidato.sql
+++ backend/src/main/resources/db/migration/V2__add_geolocalizacao_candidato.sql
@@ -1,4 +1,8 @@
 ALTER TABLE dim_candidato
-    ADD COLUMN cep VARCHAR(10)          NULL AFTER cluster_residencia,
-    ADD COLUMN lat DECIMAL(12, 6)       NULL AFTER cep,
-    ADD COLUMN lon DECIMAL(12, 6)       NULL AFTER lat;
+ADD COLUMN cep VARCHAR(10);
+
+ALTER TABLE dim_candidato
+ADD COLUMN lat DECIMAL(12,6);
+
+ALTER TABLE dim_candidato
+ADD COLUMN lon DECIMAL(12,6);
