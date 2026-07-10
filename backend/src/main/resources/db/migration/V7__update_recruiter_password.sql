07e29955a529fb8d0befb2966a1cce5fd4f086fd
AndreRibeiro <aluizrr@yahoo.com.br>
2026-07-10 20:53:31 -0300

    fix(migration): remove arquivo de migração duplicado da versão 7

--- backend/src/main/resources/db/migration/V7__update_recruiter_password.sql
+++
@@ -1,7 +0,0 @@
--- V7: Atualiza a senha do usuário recrutador padrão para uma mais forte
--- Nova senha em texto claro: Recrutador@Bit2026!
--- Hash gerada via Pbkdf2PasswordEncoder: ecd91a55f5c1d9d83a0d9ad9dfe7728ed2436b30a5bfa82f52a0f410d643aa65d9e7f6b27526d470f5c7fbac5f4f44b9
-
-UPDATE dim_usuario
-SET senha_hash = 'ecd91a55f5c1d9d83a0d9ad9dfe7728ed2436b30a5bfa82f52a0f410d643aa65d9e7f6b27526d470f5c7fbac5f4f44b9'
-WHERE email = 'recrutador@appbit.com.br';
